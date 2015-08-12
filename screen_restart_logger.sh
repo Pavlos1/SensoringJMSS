8dd7762752b06d0ce36bbac278bb42f40b3f8ee1
Pavel Zakopaylo <zak0005@jmss.vic.edu.au>
2015-08-12 13:14:40 +1000

    (Pavel) Moved arduino_flasher to git repo

---
+++ screen_restart_logger.sh
@@ -0,0 +1,3 @@
+screen -S data_logger -X quit
+/home/root/GalileoWebServer/screen_logger.sh
+
--- startlogger.sh
+++ startlogger.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 /sbin/modprobe cdc-acm
-cd /home/root/arduino_flasher
+cd /home/root/GalileoWebServer/arduino_flasher
 ./flash_arduino current.hex /dev/`ls /dev | grep -i acm | head -n 1` 115200
-#sleep 30
 cd /home/root/GalileoWebServer
 /home/root/GalileoWebServer/read_arduino_data.py
