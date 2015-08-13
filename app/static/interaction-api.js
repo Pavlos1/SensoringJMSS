396c3143cfd7f276f6352a548750652f9d026dc1
Saurabh Joshi <jos0003@jmss.vic.edu.au>
2015-08-14 09:49:31 +1000

    zoom buttons work now? attempt 2

--- app/static/interaction-api.js
+++ app/static/interaction-api.js
@@ -21,11 +21,11 @@ document.getElementById('restore').addEventListener('click', function() {
 });
 
 document.getElementById('zoomin').addEventListener('click', function() {
-    zoom(g, 3/2 * 100, 0, 0);
+    zoom(g, 50, 0, 0);
 });
 
 document.getElementById('zoomout').addEventListener('click', function() {
-    zoom(g, 2/3 * 100, 0, 0);
+    zoom(g, -50, 0, 0);
 });
 
 
