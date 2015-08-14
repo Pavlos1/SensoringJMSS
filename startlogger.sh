04aeaba1c428d4e0fc9da9120139f0cb180ba979
Pavel Zakopaylo <zak0005@jmss.vic.edu.au>
2015-08-14 13:46:59 +1000

    (Pavel) We can now read errors if screen dies.

--- startlogger.sh
+++ startlogger.sh
@@ -4,3 +4,5 @@ cd /home/root/GalileoWebServer/arduino_flasher
 ./flash_arduino current.hex /dev/`ls /dev | grep -i acm | head -n 1` 115200
 cd /home/root/GalileoWebServer
 /home/root/GalileoWebServer/read_arduino_data.py
+/bin/bash
+
