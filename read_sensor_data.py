6d9821fb92fc22a7c2a17696cf7b6d5f08e8cf93
Pavel Zakopaylo <zak0005@jmss.vic.edu.au>
2015-10-09 10:00:18 +1100

    More work on direct sensor interface

--- read_sensor_data.py
+++ read_sensor_data.py
@@ -4,22 +4,35 @@
 
 import sqlite3
 import time
+import os
 import mraa
 
 light = mraa.Aio(0)
 sound = mraa.Aio(1)
 temp = mraa.I2c(0)
 temp.address(...)
+error_count = 0
 
 while True:
-    light_t = 0
-    sound_t = 0
-    temp_t = 0
-    for i in range(0, 50):
-        light_t += light.read()
-        sound_t += sound.read()
-        temp_t = ...
-    con = ...
-    with con:
-        cur = ...
-        cur.exec...
+    try:
+        light_t = 0
+        sound_t = 0
+        temp_t = 0
+        for i in range(0, 50):
+            light_t += light.read()
+            sound_t += sound.read()
+            temp_t += ...
+        con = sqlite3.connect("sensor_data.db")
+        with con:
+            cur = con.cursor()
+            cur.exec("insert into data (%d,%d,%d,%f,%f)" %(int(time.time()), int(light_t/50), int(sound_t/50), 
+temp_t/50, 0))
+        con.close()
+        error_count = 0
+        time.sleep(0.1)
+    except:
+        if error_count < 20:
+            print "Something went wrong. Continuing... %d" %error_count
+        else:
+            print "error_count >= 20. Bailing..."
+            os.system("/sbin/reboot")
