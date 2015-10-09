48fe44d45c0464d2a649ed5e47335dd5d18bd390
Pavel Zakopaylo <zak0005@jmss.vic.edu.au>
2015-10-09 15:35:42 +1100

    Finished direct sensor code

--- read_sensor_data.py
+++ read_sensor_data.py
@@ -10,7 +10,8 @@ import mraa
 light = mraa.Aio(0)
 sound = mraa.Aio(1)
 temp = mraa.I2c(0)
-temp.address(...)
+i2c.address(0x49)
+i2c.writeWordReg(1, 0b1011000001100000)
 error_count = 0
 
 while True:
@@ -21,12 +22,21 @@ while True:
         for i in range(0, 50):
             light_t += light.read()
             sound_t += sound.read()
-            temp_t += ...
+            raw_read = i2c.readWordReg(0)
+            msb = raw_read & 0b11111111
+            lsb = (raw_read >> 8)
+            if (lsb & 1) == 1:
+                lsb >>= 3
+                msb <<= 5
+            else:
+                lsb >>= 4
+                msb <<= 4
+            correct_read = msb | lsb
+        temp_t += (correct_read * 0.0625)
         con = sqlite3.connect("sensor_data.db")
         with con:
             cur = con.cursor()
-            cur.exec("insert into data (%d,%d,%d,%f,%f)" %(int(time.time()), int(light_t/50), int(sound_t/50), 
-temp_t/50, 0))
+            cur.exec("insert into data (%d,%d,%d,%f,%f)" %(int(time.time()), int(light_t/50), int(sound_t/50), temp_t/50, 0))
         con.close()
         error_count = 0
         time.sleep(0.1)
@@ -35,4 +45,5 @@ temp_t/50, 0))
             print "Something went wrong. Continuing... %d" %error_count
         else:
             print "error_count >= 20. Bailing..."
-            os.system("/sbin/reboot")
+            raise SystemExit
+            #os.system("/sbin/reboot")
