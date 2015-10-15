c1a8f96c4a864a475c3862a8fe05a21263358e73
Pavel Zakopaylo <zakopaylo2@gmail.com>
2015-10-15 15:57:53 +1100

    Removed startlogger.sh

--- startlogger.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-/sbin/modprobe cdc-acm
-cd /home/root/GalileoWebServer/arduino_flasher
-./flash_arduino current.hex /dev/`ls /dev | grep -i acm | head -n 1` 115200
-cd /home/root/GalileoWebServer
-/home/root/GalileoWebServer/read_arduino_data.py
-/bin/bash
-
