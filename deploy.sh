a480f17564dff9ec799e0f4d77b0439022ed0cc0
Pavel Zakopaylo <zakopaylo2@gmail.com>
2015-11-20 17:04:42 +1100

    Fixed error in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-push git@github.com:Pavlos1/SensoringJMSS.git master
+git push git@github.com:Pavlos1/SensoringJMSS.git master
 git push root@118.139.125.200:GalileoWebServer.git master
 git push root@118.139.125.117:GalileoWebServer.git master
